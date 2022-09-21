299225f3812a6fdd3ba31c8741848d49bec1d482
vegas007gof <vegas007gof@mail.ru>
2022-09-22 02:45:58 +0300

    1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LR_One
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y;
+            int X = 7;
+
+            y = 555;
+
+            Console.WriteLine(y);
+            Console.WriteLine(X);
+            Console.WriteLine("Турклиев Владимир");
+            Console.WriteLine("Лабораторная работа #1");
+            Console.WriteLine("ПИЖ-б-о-21-1");
+            Console.WriteLine("02/08/2002");
+            Console.WriteLine("Ставрополь");
+            Console.WriteLine("Физика");
+            Console.WriteLine("Теннис");
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace LR12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 3.4;
+            double z = 5;
+            double Et = 8.2;
+            double t = (35 / a * z + z * a - (a + Et) / 4);
+            Console.WriteLine(t);
+
+        Console.ReadKey();
+        }
+    }
+}
